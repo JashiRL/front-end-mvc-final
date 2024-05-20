c679b642048945e3a3ca216896d616a0c58f8b91
LoiLoL421 <j.robleslecona@ugto.mx>
2024-05-20 08:35:04 -0600

    try to fix

--- components/footer-general.vue
+++ components/footer-general.vue
@@ -4,8 +4,8 @@
       <h2>Newsletter</h2>
     </div>
     <v-row align="center" justify="center">
-      <v-form ref="form"  lazy-validation>
-        <v-text-field   label="E-mail" required>
+      <v-form ref="form" lazy-validation>
+        <v-text-field label="E-mail" required>
           <template v-slot:append>
             <v-btn depressed  color="success">Aceptar</v-btn>
           </template>
