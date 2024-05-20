548a202fbe0b744951693f078f30e16ef515dc98
LoiLoL421 <j.robleslecona@ugto.mx>
2024-05-20 00:33:46 -0600

    algunos avances en la estructura del proyecto y unas cuantas preparaciones para el futuro del proyecto

---
+++ components/footer-grl.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-container>
+    <div class="text-center">
+      <h2>Newsletter</h2>
+    </div>
+    <v-row align="center" justify="center">
+      <v-form ref="form"  lazy-validation>
+        <v-text-field   label="E-mail" required>
+          <template v-slot:append>
+            <v-btn depressed  color="success">Aceptar</v-btn>
+          </template>
+        </v-text-field>
+      </v-form>
+    </v-row>
+  </v-container>
+</template>
---
+++ components/header-gral.vue
@@ -0,0 +1,25 @@
+<!-- Header.vue -->
+<template>
+  <header>
+    <div class="titulo">
+      <h1>Mi Encabezado</h1>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'hea-ders',
+  data () {
+    return {
+      tittle: 'Candleleaf'
+    }
+  }
+}
+</script>
+
+<style scoped>
+.titulo{
+  background-color:aliceblue;
+}
+</style>
